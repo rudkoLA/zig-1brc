0d33f31928809867d96a82989ffc1c8e18dc9416
rudkoLA <rudko.adam.1@gmail.com>
2026-08-03 22:56:33 +0300

    add basic single threaded 1brc

---
+++ build.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+
+    const is_fast = b.option(bool, "fast", "Build with ReleaseFast optimization") orelse false;
+
+    const optimize: std.builtin.OptimizeMode = if (is_fast) .ReleaseFast else b.standardOptimizeOption(.{});
+
+    const executables = [_][]const u8{
+        "1brc_1",
+    };
+
+    for (executables) |exe_name| {
+        const src_path = b.fmt("src/{s}.zig", .{exe_name});
+
+        const exe = b.addExecutable(.{
+            .name = exe_name,
+            .root_module = b.createModule(.{
+                .root_source_file = b.path(src_path),
+                .target = target,
+                .optimize = optimize,
+            }),
+        });
+
+        b.installArtifact(exe);
+    }
+}
+
+
